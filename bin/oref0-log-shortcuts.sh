67519573d7c35c67301c2bd0ada92b45813082bb
Alim <alimhassam@gmail.com>
2017-10-30 22:07:04 -0400

    WIP; allow different editor

--- bin/oref0-log-shortcuts.sh
+++ bin/oref0-log-shortcuts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+EDITOR=${$EDITOR:-nano}
 # add crontab entries
 grep -q networklog ~/.bash_profile 2>/dev/null || echo "alias networklog="'"tail -n 100 -F /var/log/openaps/network.log"' >> ~/.bash_profile
 grep -q xdrip-looplog ~/.bash_profile || echo "alias xdrip-looplog="'"tail -n 100 -F /var/log/openaps/xdrip-loop.log"' >> ~/.bash_profile
@@ -10,11 +11,11 @@ grep -q pump-looplog ~/.bash_profile || echo "alias pump-looplog="'"tail -n 100 
 grep -q urchin-looplog ~/.bash_profile || echo "alias urchin-looplog="'"tail -n 100 -F /var/log/openaps/urchin-loop.log"' >> ~/.bash_profile
 grep -q ns-looplog ~/.bash_profile || echo "alias ns-looplog="'"tail -n 100 -F /var/log/openaps/ns-loop.log"' >> ~/.bash_profile
 grep -q cat-pref ~/.bash_profile || echo "alias cat-pref="'"cd ~/myopenaps && cat preferences.json"' >> ~/.bash_profile
-grep -q edit-pref ~/.bash_profile || echo "alias edit-pref="'"cd ~/myopenaps && nano preferences.json"' >> ~/.bash_profile
+grep -q edit-pref ~/.bash_profile || echo "alias edit-pref="'"cd ~/myopenaps && $EDITOR preferences.json"' >> ~/.bash_profile
 grep -q cat-wifi ~/.bash_profile || echo "alias cat-wifi="'"cat /etc/wpa_supplicant/wpa_supplicant.conf"' >> ~/.bash_profile
-grep -q edit-wifi ~/.bash_profile || echo "alias edit-wifi="'"nano /etc/wpa_supplicant/wpa_supplicant.conf"' >> ~/.bash_profile
+grep -q edit-wifi ~/.bash_profile || echo "alias edit-wifi="'"$EDITOR /etc/wpa_supplicant/wpa_supplicant.conf"' >> ~/.bash_profile
 grep -q cat-runagain ~/.bash_profile || echo "alias cat-runagain="'"cd ~/myopenaps && cat oref0-runagain.sh"' >> ~/.bash_profile
-grep -q edit-runagain ~/.bash_profile || echo "alias edit-runagain="'"cd ~/myopenaps && nano oref0-runagain.sh"' >> ~/.bash_profile
+grep -q edit-runagain ~/.bash_profile || echo "alias edit-runagain="'"cd ~/myopenaps && $EDITOR oref0-runagain.sh"' >> ~/.bash_profile
 grep -q cat-autotune ~/.bash_profile || echo "alias cat-autotune="'"cd ~/myopenaps/autotune && cat autotune_recommendations.log"' >> ~/.bash_profile
 grep -q git-branch ~/.bash_profile || echo "alias git-branch="'"cd ~/src/oref0 && git branch"' >> ~/.bash_profile
 grep -q runagain ~/.bash_profile || echo "alias runagain="'"bash ~/myopenaps/oref0-runagain.sh"' >> ~/.bash_profile
